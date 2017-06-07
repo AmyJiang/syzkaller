9753934b3d0f06e6259f4efdefc1cc33d47d4e39
Ruoxin Jiang <rj2394@columbia.edu>
2017-06-07 15:26:51 -0400

    reprodiff: reproduce/minimize diff, output a log

--- tools/syz-reprodiff/reprodiff.go
+++ tools/syz-reprodiff/reprodiff.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 	"time"
 
+	"github.com/google/syzkaller/fileutil"
 	"github.com/google/syzkaller/ipc"
 	. "github.com/google/syzkaller/log"
 	"github.com/google/syzkaller/prog"
@@ -15,10 +18,13 @@ import (
 
 var (
 	flagTestfs   = flag.String("testfs", "./", "a colon-separated list of test filesystems")
+	flagLog      = flag.String("log", "repro.log", "reproducing log")
 	flagExecutor = flag.String("executor", "./syz-executor", "path to executor binary")
 	flagProg     = flag.String("prog", "", "diff-inducing program to reproduce")
 	flagMinimize = flag.Bool("min", false, "minimize input program")
+	flagSaveDir  = flag.Bool("save", false, "save working directory")
 	testfs       []string
+	logFile      *os.File
 )
 
 func initExecutor() (*ipc.Env, error) {
@@ -35,28 +41,30 @@ func initExecutor() (*ipc.Env, error) {
 	return env, nil
 }
 
-func execute1(env *ipc.Env, prog *prog.Prog) ([][]uint32, error) {
+func execute1(env *ipc.Env, prog *prog.Prog) ([][]uint32, []string, error) {
 	// FIXME: error, failed, hanged?
 	states := make([][]uint32, len(testfs))
-	for i, dir := range testfs {
-		output, _, failed, hanged, err, state := env.Exec(prog, false, false, true, dir)
+	workdirs := make([]string, len(testfs))
+	for i, fs := range testfs {
+		output, _, failed, hanged, err, state, dir := env.Exec(prog, false, false, true, fs)
 		states[i] = append(states[i], state...)
+		workdirs[i] = dir
 		if err != nil {
 			Logf(0, "ERR: executor threw error: %s", err)
-			return nil, err
+			return nil, nil, err
 		}
 
 		if failed {
 			Logf(0, "BUG: executor-detected bug:\n%s", output)
-			return nil, fmt.Errorf("executor-detected failure")
+			return nil, nil, fmt.Errorf("executor-detected failure")
 		}
 		if hanged {
 			Logf(0, "HANG: executor hanged")
-			return nil, fmt.Errorf("executor-detected hang")
+			return nil, nil, fmt.Errorf("executor-detected hang")
 		}
 	}
 
-	return states, nil
+	return states, workdirs, nil
 }
 
 func parseInput(inputFile string) (*prog.Prog, error) {
@@ -75,9 +83,8 @@ func parseInput(inputFile string) (*prog.Prog, error) {
 
 func isDiscrepancy(states [][]uint32) bool {
 	for i := 1; i < len(states); i += 1 {
-		// FIXME: executor fails before handle_completion
 		if len(states[i]) != len(states[i-1]) {
-			break
+			return true
 		}
 		for j, v := range states[i-1] {
 			if v != states[i][j] {
@@ -88,10 +95,52 @@ func isDiscrepancy(states [][]uint32) bool {
 	return false
 }
 
+var header = "ReproDiff: %s\n=====================================\n"
+
+func writeLog(template string, vargs ...interface{}) error {
+	if logFile == nil {
+		return fmt.Errorf("failed to write to log file")
+	}
+
+	str := fmt.Sprintf(template, vargs...)
+	_, err := logFile.WriteString(str)
+	if err != nil {
+		return fmt.Errorf("failed to write to log file")
+	}
+	return nil
+}
+
+func writeFsStates(workdirs []string) error {
+	defer func() {
+		if *flagSaveDir {
+			return
+		}
+		for _, dir := range workdirs {
+			fileutil.UmountAll(dir)
+			os.RemoveAll(dir)
+		}
+	}()
+
+	for i, dir := range workdirs {
+		if err := writeLog("### %s\n", testfs[i]); err != nil {
+			return err
+		}
+		out, err := exec.Command("/bin/ls", "-lR", filepath.Join(dir, "0")).Output()
+		if err != nil {
+			return fmt.Errorf("failed to retrieve %v state: %v", testfs[i], err)
+		}
+		if err := writeLog("%s\n\n", out); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func reproduce() error {
+	defer logFile.Close()
+
 	env, err := initExecutor()
 	defer env.CloseWithoutRm()
-
 	if err != nil {
 		return err
 	}
@@ -101,21 +150,43 @@ func reproduce() error {
 		return err
 	}
 
-	states, err := execute1(env, p)
+	if p == nil || len(p.Calls) == 0 {
+		Logf(0, "received test program")
+		if err := writeLog(header, "Test VM Passed"); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	Logf(0, "received program to reproduce: %s", p)
+	if err := writeLog(header, filepath.Base(*flagProg)); err != nil {
+		return err
+	}
+	if err := writeLog("## Prog: %s\n%s\n\n## State:\n", p, p.Serialize()); err != nil {
+		return err
+	}
+
+	states, workdirs, err := execute1(env, p)
 	if err != nil {
+		writeLog("Failed to execute program: %v\n\n", err)
 		return err
 	}
 
 	if !isDiscrepancy(states) {
+		writeLog("Failed to reproduce discrepancy: %v\n\n", err)
 		return fmt.Errorf("failed to reproduce discrepancy")
+	}
+
+	Logf(0, "reproduced program %s", p)
+	if err := writeFsStates(workdirs); err != nil {
+		return err
 	}
 
 	if !*flagMinimize {
 		return nil
 	}
-
 	p1, _ := prog.Minimize(p, -1, func(p1 *prog.Prog, call1 int) bool {
-		states, err := execute1(env, p1)
+		states, _, err := execute1(env, p1)
 		if err != nil {
 			// FIXME: how to compare in the existence of error/hang?
 			return false
@@ -124,33 +195,31 @@ func reproduce() error {
 		}
 	}, false)
 
-	Logf(0, "Minimized prog: %s", p1)
-	if err := writeFile(*flagProg+".min", p1.Serialize()); err != nil {
+	Logf(0, "minimized prog to %s", p1)
+	err = writeLog("## Minimized Prog: %s\n%s\n\n", p1, p1.Serialize())
+	if err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func writeFile(outf string, data []byte) error {
-	err := ioutil.WriteFile(outf, data, 0666)
-	return err
-}
-
 func main() {
 	flag.Parse()
 	if *flagProg == "" {
 		Fatalf("Must specify a diff-inducing program to reproduce")
-		os.Exit(1)
 	}
 
 	testfs = strings.Split(*flagTestfs, ":")
 	if len(testfs) < 2 {
 		Fatalf("Must specify two or more test filesystems")
-		os.Exit(1)
 	}
 
-	Logf(0, "Test directories: %v", testfs)
+	var err error
+	logFile, err = os.OpenFile(*flagLog, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		Fatalf("failed to create log file: %v", err)
+	}
 
 	if err := reproduce(); err != nil {
 		Fatalf("Failed to reproduce: %s", err)
